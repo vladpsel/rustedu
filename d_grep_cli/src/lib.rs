c56596041a1eaa53316acf30d25d4f31d0ac916f
Vlad <vpanovwr@gmail.com>
2026-03-10 00:38:35 +0200

    update(grep): update files

---
+++ d_grep_cli/src/lib.rs
@@ -0,0 +1,49 @@
+use std::fs;
+
+pub struct Config {
+    pub flag: String,
+    pub files_path: String,
+    pub search: String,
+    pub case_ignore: bool,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() <= 1 || args.len() > 4 {
+            return Err("Invalid argument count");
+        }
+
+        let flag = String::from(&args[1]);
+        let mut case_ignore: bool = false;
+
+        if flag == "-h" || flag == "-help" {
+            print!(
+                "
+                CLI Tool for search in files. \r\n
+                -h or -heplp for this info message \r\n
+                -i or --ignore-case for ignore search case \r\n
+                "
+            );
+            return Err("");
+        }
+
+        if flag == "-i" || flag == "--ignore-case" {
+            case_ignore = true;
+        }
+
+        let files_path = args[2].clone();
+        let search = args[3].clone();
+
+        Ok(Self {
+            flag,
+            files_path,
+            search,
+            case_ignore,
+        })
+    }
+
+    pub fn find(&self) {
+        let file = fs::read_to_string(&self.files_path).unwrap();
+        println!("{}", file);
+    }
+}
---
+++ d_grep_cli/src/main.rs
@@ -0,0 +1,12 @@
+use d_grep_cli::Config;
+use std::{env, process};
+
+fn main() {
+    let arguments: Vec<String> = env::args().collect();
+    let app = Config::build(&arguments).unwrap_or_else(|err| {
+        println!("{err}");
+        process::exit(0);
+    });
+
+    app.find();
+}
